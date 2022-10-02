df512cbc4e1c3321a7e3577e181492cb6e3b090d
Abhishek <abhishekchauhan85805@gmail.com>
2022-10-02 18:18:17 +0530

    files added

---
+++ 10multiset.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(multiset<string> &s){
+    for(string values:s){
+        cout<<values<<" ";
+    }
+    cout<<endl<<endl;
+    multiset<string>::iterator it;
+    for(it=s.begin();it!=s.end();it++){
+        cout<<*it<<" ";
+    }
+    cout<<endl<<endl;
+}
+
+int main(){
+    multiset<string> s;
+    s.insert("abc");
+    s.insert("def");
+    s.insert("ghi");
+    s.insert("abc");
+    print(s);
+
+    auto it = s.find("abc");
+    if(it==s.end())
+        cout<<"No value"<<endl;
+    else    
+        cout<<*it<<endl;
+    cout<<endl;
+
+    s.erase(it);
+    //s.erase("abc"); // erase both "abc"
+    print(s);
+
+    return 0;
+}
---
+++ 9unorderedSet.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(unordered_set<string> &s){
+    for(string values:s){
+        cout<<values<<" ";
+    }
+    cout<<endl<<endl;
+    unordered_set<string>::iterator it;
+    for(it=s.begin();it!=s.end();it++){
+        cout<<*it<<" ";
+    }
+    cout<<endl<<endl;
+}
+
+int main(){
+    unordered_set<string> s;
+    s.insert("abc");
+    s.insert("def");
+    s.insert("ghi");
+    s.insert("jkl");
+    print(s);
+
+    auto it = s.find("jkl");
+    if(it==s.end())
+        cout<<"No value"<<endl;
+    else    
+        cout<<*it<<endl;
+    cout<<endl;
+
+    s.erase(it);
+    s.erase("abc");
+    print(s);
+
+    return 0;
+}
